5e4798ba4da4d75ec8e6e3ee77cfd47275f5b3f4
Mori Yoshizumi <viimaple1001@gmail.com>
2024-11-07 11:36:02 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2024 Ryuichi Ueda <Viimaple1001@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -8,9 +8,18 @@ ng () {
 }
 
 res=0
-
+###正常な入力###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+###変な入力###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 
