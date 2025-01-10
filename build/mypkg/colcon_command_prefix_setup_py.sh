b01c15097d34faf49e39464008db86ea7a4ffb4b
Viimaple <Viimaple1001@gmail.com>
2025-01-11 06:30:13 +0900

    Delete build directory

--- build/mypkg/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/mori/ros2_ws/install/person_msgs/share/person_msgs/package.sh"
--- build/person_msgs/person_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mori/ros2_ws/build/person_msgs/person_msgs__py
-# Build directory: /home/mori/ros2_ws/build/person_msgs/person_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
